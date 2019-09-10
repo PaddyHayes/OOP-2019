6ec98131a2937e86a2ea4e02939f9ef3972ac93f
PaddyHayes <55146192+PaddyHayes@users.noreply.github.com>
2019-09-10 17:36:27 +0100

    Add files via upload

--- src/Set0.java
+++ src/Set0.java
@@ -3,11 +3,21 @@ import java.util.Scanner;
 public class Set0 {
     public static void main(String args[]) {
 
-        String Name;
-        float ClassName;
-        int price = 2, snacks;
+        String Name, ClassName;
+        int price=2, snacks;
 
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter your name: ");
+        Name = input.nextLine();
+
+        System.out.print("Please enter your class name: ");
+        ClassName = input.nextLine();
+
+        System.out.print("Please select the number of snacks you would like: ");
+        snacks = input.nextInt();
+
+        System.out.println("Name: " + Name + "\nClass: " + ClassName + "\nNo.Of Snacks: " + snacks + "\nPrice: " + snacks*price);
 
 
     }
