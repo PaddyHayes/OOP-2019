c0c7d9b298402b015d18f64a11db5d6c9fb807b4
PaddyHayes <55146192+PaddyHayes@users.noreply.github.com>
2019-09-10 17:11:19 +0100

    Add files via upload

---
+++ src/Set0.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Set0 {
+    public static void main(String args[]) {
+
+        String Name;
+        float ClassName;
+        int price = 2, snacks;
+
+        Scanner input = new Scanner(System.in);
+
+
+    }
+}
